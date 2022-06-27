ce1122a1aede1c182afc950373242372a9f2adb1
4g1v3m3 <leb334818@gmail.com>
2022-06-27 19:58:35 +0600

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const { reset } = require("colors");
+
 module.exports = function reverse (n) {
-  
+    k = n.toString()
+    let strReverse = k.split('').reverse().join('').replace('-', '')
+    return strReverse
 }
